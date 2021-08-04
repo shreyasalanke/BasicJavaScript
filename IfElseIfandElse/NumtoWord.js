8e012c8a3420d6d487182fa2082fc7ae0eb5fb07
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-04 16:25:22 +0530

    ADDED: program to convert single digit number to word

---
+++ IfElseIfandElse/NumtoWord.js
@@ -0,0 +1,25 @@
+let number = Math.floor(Math.random() * 10);
+let num;
+if( number == 0) {
+    num = 'zero';
+} else if (number == 1) {
+    num = 'one';
+}else if (number == 2) {
+    num ='two';
+}else if (number == 3) {
+    num ='three';
+}else if (number == 4) {
+    num ='four';
+}else if (number == 5) {
+    num ='five';
+}else if (number == 6) {
+    num ='six';
+}else if (number == 7) {
+    num ='seven';
+}else if (number == 8) {
+    num ='eight';
+}else if (number == 9) {
+    num ='nine';
+}
+
+console.log('Ouput is : '+num);
